5844e80aa52757bc351dd83ed70f826cc6fd1505
Steven Shirley <stevenshirley@Stevens-MacBook-Pro.local>
2019-03-30 12:49:49 -0500

    touched up on font size on mobile

--- javascript/app3.js
+++ javascript/app3.js
@@ -2,6 +2,7 @@ window.addEventListener("scroll", handleScroll, false);
 
 var body = document.getElementsByTagName("body")[0];
 var navBar = document.getElementsByClassName("masterhead")[0];
+var portItems = document.getElementsByClassName("portitem");
 var footIcons = document.getElementsByClassName("footicons")[0];
 
 function sleep(ms) {
@@ -9,7 +10,13 @@ function sleep(ms) {
 }
 
 window.onload = async function() {
-  body.style.opacity = 1;
+    body.style.opacity = 1;
+
+    for (var i = 0; i < portItems.length; i++) {
+        if (window.scrollY + window.innerHeight > portItems[i].offsetTop + portItems[i].clientHeight / 4) {
+            portItems[i].classList.add("visible");
+        }
+    }
 }
 function handleScroll(event) {
     if (window.scrollY > 500) {
@@ -17,6 +24,12 @@ function handleScroll(event) {
     }
     else {
          navBar.classList.remove("scrolled");
+    }
+
+    for (var i = 0; i < portItems.length; i++) {
+        if (window.scrollY + window.innerHeight > portItems[i].offsetTop + portItems[i].clientHeight / 4) {
+            portItems[i].classList.add("visible");
+        }
     }
 
     for (var i = 0; i < footIcons.children.length; i++) {
