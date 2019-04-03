479877c724ed02f2b74ae1edc3946c8e1ae60158
Steven Shirley <stevenshirley@Stevens-MacBook-Pro.local>
2019-04-03 14:11:43 -0500

    touched up on style

--- javascript/app.js
+++ javascript/app.js
@@ -1,26 +1,35 @@
 window.addEventListener("scroll", handleScroll, false);
 
-var viewProjects = document.getElementById("introname");
+// navbar and image fading for landing screen
 var navBar = document.getElementsByClassName("masterhead")[0];
-
 var landingPageFader = document.getElementById("homecont");
 
-var portItems = document.getElementsByClassName("portitem");
-var sendMessage = document.getElementById("homequotebtn");
-var readMore = document.getElementByClassName("readmorebtn");
-var viewResume = document.getElementByClassName("resumebtn");
-var footIcons = document.getElementsByClassName("footicons")[0];
-
-var loadingPage = document.getElementById("loadingpage");
+// background image and title text fade in
 var homePage = document.getElementById("homecont");
 var homeElements = homePage.children;
 
+// children of <body> to display after loading screen finishes
 var portTitle = document.getElementById("homeporttitle");
 var portButtons = document.getElementById("homeportbtns");
 var homePort = document.getElementById("homeport");
 var homeQuote = document.getElementById("homequote");
-var homeAbout = document.getElementById("homeabout");
+var homeAbout = document.getElementsByClassName("homeabout")[0];
+var skillTitle = document.getElementById("skillstitle1");
+var skillSubTitle = document.getElementById("skillstitle2");
+var skills = document.getElementById("skillscontainer");
+var otherHomeAbout = document.getElementsByClassName("homeabout")[1];
 var footer = document.getElementsByClassName("masterfoot")[0];
+
+// buttons to fade in when we scroll to them
+var viewProjects = document.getElementById("introname");
+var portItems = document.getElementsByClassName("portitem");
+var sendMessage = document.getElementById("homequotebtn");
+var skillItems = document.getElementsByClassName("skillitem");
+var viewResume = document.getElementById("resumebtn");
+var footIcons = document.getElementsByClassName("footicons")[0];
+
+// loading page animations
+var loadingPage = document.getElementById("loadingpage");
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -32,6 +41,10 @@ window.onload = async function() {
     homePort.style.display = "none";
     homeQuote.style.display = "none";
     homeAbout.style.display = "none";
+    skillTitle.style.display = "none";
+    skillSubTitle.style.display = "none";
+    skills.style.display = "none";
+    otherHomeAbout.style.display = "none";
     footer.style.display = "none";
 
     navBar.style.opacity = 0;
@@ -60,7 +73,9 @@ window.onload = async function() {
         portItems[i].classList.remove("visible");
     }
     sendMessage.classList.remove("visible");
-    readMore.classList.remove("visible");
+    for (var i = 0; i < skillItems.length; i++) {
+        skillItems[i].classList.remove("visible");
+    }
     viewResume.classList.remove("visible");
     for (var i = 0; i < footIcons.children.length; i++) {
         footIcons.classList.remove("visible");
@@ -71,6 +86,10 @@ window.onload = async function() {
     homePort.style.display = "";
     homeQuote.style.display = "";
     homeAbout.style.display = "";
+    skillTitle.style.display = "";
+    skillSubTitle.style.display = "";
+    skills.style.display = "";
+    otherHomeAbout.style.display = "";
     footer.style.display = "";
 
 }
@@ -99,12 +118,17 @@ function handleScroll(event) {
     if (window.scrollY + window.innerHeight > sendMessage.offsetTop + sendMessage.clientHeight) {
         sendMessage.classList.add("visible");
     }
-    if (window.scrollY + window.innerHeight > readMore.offsetTop + readMore.clientHeight) {
-        readMore.classList.add("visible");
+
+    for (var i = 0; i < skillItems.length; i++) {
+        if (window.scrollY + window.innerHeight > skillItems[i].offsetTop + skillItems[i].clientHeight / 4) {
+            skillItems[i].classList.add("visible");
+        }
     }
+
     if (window.scrollY + window.innerHeight > viewResume.offsetTop + viewResume.clientHeight) {
         viewResume.classList.add("visible");
     }
+
     for (var i = 0; i < footIcons.children.length; i++) {
         if (window.scrollY + window.innerHeight > footIcons.children[i].children[0].offsetTop + footIcons.children[i].children[0].clientHeight) {
             footIcons.classList.add("visible");
