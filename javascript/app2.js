c1be58102b0d24be5a98b297d3fdca8da0be6c04
Steven Shirley <stevenshirley@Stevens-MBP.attlocal.net>
2019-04-08 15:19:53 -0500

    update

---
+++ javascript/app2.js
@@ -0,0 +1,36 @@
+window.addEventListener("scroll", handleScroll, false);
+
+// navbar and image fading for landing screen
+var navBar = document.getElementsByClassName("masterhead")[0];
+var landingPageFader = document.getElementsByClassName("portitemcont")[0];
+
+// background image and title text fade in
+var homePage = document.getElementsByClassName("portitemcont")[0];
+var homeElements = homePage.children;
+
+// buttons to fade in when we scroll to them
+var viewProjects = document.getElementsByClassName("portintrotitle")[0];
+var footIcons = document.getElementsByClassName("footicons")[0];
+
+function handleScroll(event) {
+    if (window.scrollY > viewProjects.offsetTop) {
+        navBar.classList.add("scrolled");
+    }
+    else {
+         navBar.classList.remove("scrolled");
+    }
+
+    if (window.scrollY < window.innerHeight) {
+        var opacity = (3 / 4) * window.scrollY / landingPageFader.clientHeight;
+        landingPageFader.style.backgroundColor = "rgba(255, 255, 255, " + opacity + ")";
+
+        var offset = (3 / 4) * window.scrollY;
+        homePage.style.paddingTop = offset + "px";
+    }
+
+    for (var i = 0; i < footIcons.children.length; i++) {
+        if (window.scrollY + window.innerHeight > footIcons.children[i].children[0].offsetTop + footIcons.children[i].children[0].clientHeight) {
+            footIcons.classList.add("visible");
+        }
+    }
+}
